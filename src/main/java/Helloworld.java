48995db33b20a634933a938231298188e8dc9d1c
huahaoran <huahaoran@patsnap.com>
2019-11-25 20:25:54 +0800

    add pom.xml

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,10 @@
+/**
+ * @author Haoran.Hua
+ * @Description: TODO
+ * @date 2019/11/25 8:24 下午
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
